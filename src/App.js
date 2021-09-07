a9925a8aff206ae69a33704c18a74979d17b28a8
Martin Najle <martinnajle@Martins-MBP.fibertel.com.ar>
2021-09-07 11:35:47 -0300

    more refactor

--- src/App.js
+++ src/App.js
@@ -4,31 +4,34 @@ import './App.css';
 const NUMBER_OF_CELLS = 9;
 let initialBoard = Array(NUMBER_OF_CELLS).fill(null);
 const boardWinners = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]];
+const PLAYERS = { ONE: 'ONE', TWO: 'TWO' };
 
 function App() {
-  let [isPlayerOne, setPlayerOne] = useState(true);
+  let [player, togglePlayer] = useState(PLAYERS.ONE);
   let [number, setNumber] = useState(0);
-  let [winner, isWinner] = useState(false);
+  let [winner, isWinner] = useState(null);
   let [board, setBoard] = useState(initialBoard);
 
   const isOver = () => number === NUMBER_OF_CELLS || winner;
 
-  const Result = ({ winner }) => !winner ? null : (<p className={`winner ${isPlayerOne ? 'blue' : 'red'}`}>WINNER</p>);
+  const Result = ({ winner }) => !winner ? null : (<p className={`winner ${player === PLAYERS.ONE ? 'blue' : 'red'}`}>WINNER</p>);
 
   const Cell = ({ index }) => {
     let aClass = '';
     if (board[index]) {
-      aClass = (board[index] === 'O') ? 'blueCircle' : 'redCircle';
+      aClass = (board[index] === PLAYERS.ONE) ? 'blueCircle' : 'redCircle';
     }
     const handleClick = () => {
       if (!board[index]) {
         const newBoard = [...board];
-        newBoard[index] = isPlayerOne ? 'X' : 'O';
+        newBoard[index] = player;
         setBoard(newBoard);
-        const aWinner = Winner(newBoard);
-        isWinner(aWinner);
-        setPlayerOne(!isPlayerOne);
-        setNumber(number + 1);
+        if (playerWon(newBoard, player)) {
+          isWinner(true);
+        } else {
+          togglePlayer((player === PLAYERS.ONE ? PLAYERS.TWO : PLAYERS.ONE));
+          setNumber(number + 1);
+        }
       }
       return;
     }
@@ -38,14 +41,10 @@ function App() {
       </button>)
   };
 
-  const Winner = (newBoard) => {
-    const cell = isPlayerOne ? 'X' : 'O';
-    return boardWinners.some(boardWinner =>
-    ((newBoard[boardWinner[0]] === cell) &&
-      (newBoard[boardWinner[1]] === cell) &&
-      (newBoard[boardWinner[2]] === cell))
-    )
-  };
+  //Check if the player has won
+  const playerWon = (newBoard, player) =>
+    boardWinners.some(boardWinner => boardWinner.every(el => newBoard[el] === player)
+    );
 
 
   return (
